63482141f8b5a9cfe82d8ab29750779307a84335
Bhuvanesh Singla <bhuvanesh.221ai014@nitk.edu.in>
2024-12-07 11:15:18 +0530

    CV Code updated

--- Computer Vision/yolotrack1.py
+++ Computer Vision/yolotrack1.py
@@ -1,28 +1,54 @@
 import cv2
 from ultralytics import YOLO
-from speed import SpeedEstimator
+from speed import SpeedDirEstimator
+import argparse
+
+parser = argparse.ArgumentParser(description="Video processing script.")
+parser.add_argument('--video', type=str, required=True, help="Path to the video file.")
+parser.add_argument('--isOneWay', type=str, required=True, choices=['y', 'n'], help="Is the road one-way? (y/n)")
+args = parser.parse_args()
+
+is_one_way = args.isOneWay.lower() == 'y'
 
 # Load YOLOv8 model
 model = YOLO("yolov10n.pt")
 # Initialize global variable to store cursor coordinates
-line_pts = [(0, 288), (1019, 288)]
 names = model.model.names  # This is a dictionary
+# line_pts = [(0, 288), (1019, 288)]
 
-speed_obj = SpeedEstimator(reg_pts=line_pts, names=names)
+# speed_obj = SpeedDirEstimator(reg_pts=line_pts, names=names)
+
+cap = cv2.VideoCapture(args.video)
+
+# Read the first frame to get the frame dimensions
+ret, frame = cap.read()
+if not ret:
+    print("Error: Unable to read video")
+    cap.release()
+    exit()
+
+frame_height, frame_width = frame.shape[:2]
+
+# Calculate the vertical coordinate as 5% from the bottom of the frame
+vertical_coord = int(frame_height * 0.38)
+line_pts = [(0, vertical_coord), (frame_width, vertical_coord)]
+# line_pts = [(0, 288), (1019, 288)]
+
+speed_obj = SpeedDirEstimator(reg_pts=line_pts, names=names)
 
 # Mouse callback function to capture mouse movement
-def RGB(event, x, y, flags, param):
-    global cursor_point
-    if event == cv2.EVENT_MOUSEMOVE:
-        cursor_point = (x, y)
-        print(f"Mouse coordinates: {cursor_point}")
+# def RGB(event, x, y, flags, param):
+#     global cursor_point
+#     if event == cv2.EVENT_MOUSEMOVE:
+#         cursor_point = (x, y)
+#         print(f"Mouse coordinates: {cursor_point}")
 
-# Set up the window and attach the mouse callback function
-cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
+# # Set up the window and attach the mouse callback function
+# cv2.namedWindow('RGB')
+# cv2.setMouseCallback('RGB', RGB)
 
 # Open the video file or webcam feed
-cap = cv2.VideoCapture('traffic.mp4')
+cap = cv2.VideoCapture(args.video)
 
 count = 0
 while True:
@@ -39,10 +65,11 @@ while True:
     frame = cv2.resize(frame, (1020, 500))
     
     # Perform object tracking
-    tracks = model.track(frame, persist=True,classes=[2,7])
+    # 1: bicycle  2: car  3: motorcycle  5: bus  7: truck
+    tracks = model.track(frame, persist=True,classes=[1,2,3,5,7])
     
     
-    im0 = speed_obj.estimate_speed(frame,tracks)
+    im0 = speed_obj.estimate_speedDir(frame,tracks, is_one_way)
     
     # Display the frame with YOLOv8 results
     cv2.imshow("RGB", frame)
